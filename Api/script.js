4f492647052964248f619c67d74bc00034b0d9c7
PedramAlidust <karsite1234@gmail.com>
2024-01-31 13:38:15 +0330

    search project

---
+++ Api/script.js
@@ -0,0 +1,38 @@
+function searchPosts() {
+    const searchInput = document.getElementById('search-input').value;
+    const apiEndpoint = 'https://ostadshabake.ir/wp-json/wp/v2/posts?search=' + encodeURIComponent(searchInput);
+  
+    fetch(apiEndpoint)
+      .then(response => response.json())
+      .then(data => {
+        displayResults(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+  
+  function displayResults(posts) {
+    const resultsContainer = document.getElementById('results');
+    const notFoundMessage = document.getElementById('not-found');
+  
+    resultsContainer.innerHTML = '';
+  
+    if (posts.length === 0) {
+      notFoundMessage.style.display = 'block';
+      return;
+    }
+  
+    notFoundMessage.style.display = 'none';
+  
+    posts.forEach(post => {
+      const postTitle = post.title.rendered;
+      const postLink = post.link;
+  
+      const postElement = document.createElement('div');
+      postElement.classList.add('post');
+      postElement.innerHTML = `<p><a href="${postLink}" target="_blank">${postTitle}</a></p>`;
+      resultsContainer.appendChild(postElement);
+    });
+  }
+  
